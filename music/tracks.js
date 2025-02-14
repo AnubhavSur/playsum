9d171da05e898112624efb97409b1de914dba939
AnubhavSur <72611113+AnubhavSur@users.noreply.github.com>
2025-02-15 00:18:03 +0530

    Add files via upload

---
+++ music/tracks.js
@@ -0,0 +1,67 @@
+const tracks = [
+      {
+        trackName: 'Oh Okay',
+        artist: 'Gunna',
+        src: 'song1.mp3',
+        cover: 'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2024%2F04%2F15%2Fgunna-one-of-wun-album-announcement-cover-art-0.jpg?w=960&cbr=1&q=90&fit=max',
+        isFavorite: false
+      },
+      {
+        trackName: 'Dumb, Dumber and Dumber',
+        artist: 'Lil Baby',
+        src: 'song2.mp3',
+        cover: 'https://www.nme.com/wp-content/uploads/2021/01/nme-big-read-2021-lil-baby-cover@1990x2488.jpg',
+        isFavorite: false
+      },
+      {
+        trackName: 'Hold Me Up',
+        artist: 'Young Thug',
+        src: 'song3.mp3',
+        cover: 'https://static.wikia.nocookie.net/totalwar-ar/images/a/ac/Young_Thug.jpg/revision/latest/scale-to-width-down/450?cb=20210611170218',
+        isFavorite: false
+      },
+{
+        trackName: 'Oh Okay',
+        artist: 'Gunna',
+        src: 'song1.mp3',
+        cover: 'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2024%2F04%2F15%2Fgunna-one-of-wun-album-announcement-cover-art-0.jpg?w=960&cbr=1&q=90&fit=max',
+        isFavorite: false
+      },
+      {
+        trackName: 'Dumb, Dumber and Dumber',
+        artist: 'Lil Baby',
+        src: 'song2.mp3',
+        cover: 'https://www.nme.com/wp-content/uploads/2021/01/nme-big-read-2021-lil-baby-cover@1990x2488.jpg',
+        isFavorite: false
+      },
+      {
+        trackName: 'Hold Me Up',
+        artist: 'Young Thug',
+        src: 'song3.mp3',
+        cover: 'https://static.wikia.nocookie.net/totalwar-ar/images/a/ac/Young_Thug.jpg/revision/latest/scale-to-width-down/450?cb=20210611170218',
+        isFavorite: false
+      },
+{
+        trackName: 'Oh Okay',
+        artist: 'Gunna',
+        src: 'song1.mp3',
+        cover: 'https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2024%2F04%2F15%2Fgunna-one-of-wun-album-announcement-cover-art-0.jpg?w=960&cbr=1&q=90&fit=max',
+        isFavorite: false
+      },
+      {
+        trackName: 'Dumb, Dumber and Dumber',
+        artist: 'Lil Baby',
+        src: 'song2.mp3',
+        cover: 'https://www.nme.com/wp-content/uploads/2021/01/nme-big-read-2021-lil-baby-cover@1990x2488.jpg',
+        isFavorite: false
+      },
+      {
+        trackName: 'Hold Me Up',
+        artist: 'Young Thug',
+        src: 'song3.mp3',
+        cover: 'https://static.wikia.nocookie.net/totalwar-ar/images/a/ac/Young_Thug.jpg/revision/latest/scale-to-width-down/450?cb=20210611170218',
+        isFavorite: false
+      }
+
+
+    ];
